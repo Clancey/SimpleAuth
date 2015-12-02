ef67ee820cddd80fd111b16939050dafeca72aed
James Clancey <clancey@xamarin.com>
2015-12-01 16:12:08 -0900

    Added Basic Auth Api

---
+++ src/SimpleAuth/BasicAuth/BasicAuthApi.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAuth.BasicAuth
+{
+    public class BasicAuthApi : Api
+    {
+		protected string LoginUrl { get; set; }
+	    public BasicAuthApi(string identifier,string loginUrl, HttpMessageHandler handler = null) : base(identifier, handler)
+	    {
+		    LoginUrl = loginUrl;
+			authenticator = new BasicAuthAuthenticator(Client,loginUrl);
+	    }
+
+	    protected BasicAuthAuthenticator authenticator;
+
+		protected virtual BasicAuthAuthenticator CreateAuthenticator()
+		{
+			return authenticator;
+		}
+
+		public BasicAuthAccount CurrentBasicAccount => CurrentAccount as BasicAuthAccount;
+		public static Action<BasicAuthAuthenticator> ShowAuthenticator { get; set; }
+		protected override async Task<Account> PerformAuthenticate()
+	    {
+			var account = CurrentBasicAccount ?? GetAccount<BasicAuthAccount>(Identifier);
+			if (account?.IsValid() == true)
+		    {
+			    return account;
+		    }
+
+			authenticator = CreateAuthenticator();
+
+			ShowAuthenticator(authenticator);
+
+			var token = await authenticator.GetAuthCode();
+			if (string.IsNullOrEmpty(token))
+			{
+				throw new Exception("Null token");
+			}
+			account = new BasicAuthAccount {Key = token};
+			account.Identifier = Identifier;
+			SaveAccount(account);
+			CurrentAccount = account;
+			return account;
+		}
+
+	    protected override async Task<bool> RefreshAccount(Account account)
+	    {
+			//This should never be called. Basic auth never expires;
+		    return true;
+	    }
+
+	}
+}
---
+++ src/SimpleAuth/BasicAuth/BasicAuthAuthenticator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAuth
+{
+    public class BasicAuthAuthenticator : Authenticator
+    {
+	    readonly HttpClient client;
+	    readonly string loginUrl;
+
+	    public BasicAuthAuthenticator(HttpClient client, string loginUrl)
+	    {
+		    this.client = client;
+		    this.loginUrl = loginUrl;
+	    }
+
+	    public virtual async Task<bool> CheckCredentails(string username, string password)
+	    {
+		    if(string.IsNullOrWhiteSpace(username))
+				throw new Exception("Invalid Username");
+			if (string.IsNullOrWhiteSpace(password))
+				throw new Exception("Invalid Password");
+
+			var key = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{username}:{password}"));
+
+		    var response = await client.GetAsync(loginUrl);
+		    response.EnsureSuccessStatusCode();
+			FoundAuthCode(key);
+			return true;
+	    }
+		
+    }
+}
