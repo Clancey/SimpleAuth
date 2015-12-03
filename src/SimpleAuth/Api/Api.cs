14baa49dee4099f23267a4e803829b9bba2793c0
James Clancey <clancey@xamarin.com>
2015-12-02 15:02:36 -0900

    Added the ability to attribute Apis

--- src/SimpleAuth/Api/Api.cs
+++ src/SimpleAuth/Api/Api.cs
@@ -4,9 +4,11 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web;
 
 namespace SimpleAuth
 {
@@ -151,25 +153,83 @@ namespace SimpleAuth
 			return await Client.GetStreamAsync(new Uri(path));
 		}
 
+		public virtual Task<T> Get<T>(bool authenticated = true, [System.Runtime.CompilerServices.CallerMemberName] string methodName = "")
+		{
+			return Get<T>(null, authenticated, methodName);
+		}
+
+		public virtual Task<T> Get<T>(Dictionary<string, string> queryParameters, bool authenticated = true, [System.Runtime.CompilerServices.CallerMemberName] string methodName = "")
+		{
+			var headers = GetHeadersFromMethod(GetType().GetMethod(methodName));
+            return Get<T>(queryParameters, headers, authenticated, methodName);
+		}
+		public virtual Task<T> Get<T>(Dictionary<string, string> queryParameters, Dictionary<string, string> headers, bool authenticated = true, [System.Runtime.CompilerServices.CallerMemberName] string methodName = "")
+		{
+			var path = GetValueFromAttribute<PathAttribute>(GetType().GetMethod(methodName));
+			if (string.IsNullOrWhiteSpace(path))
+				throw new Exception("Missing Path Attribute");
+
+			path = CombineUrl(path, queryParameters);
+			return Get<T>(path, "",authenticated);
+		}
+
+		public virtual Task<T> Get<T>(string path, bool authenticated = true)
+		{
+			return Get<T>(path, null, null, authenticated);
+		}
+
+		public virtual async Task<T> Get<T>(string path, Dictionary<string, string> queryParameters,
+			Dictionary<string, string> headers, bool authenticated = true)
+		{
+			var data = await GetString(path, queryParameters, headers, authenticated);
+			return await Task.Run(() => Deserialize<T>(data));
+		}
+		public virtual Task<T> Get<T>(string path, string id, bool authenticated = true)
+		{
+			return Get<T>(Path.Combine(path, id), null, null, authenticated);
+		}
+
+
+		public virtual Task<string> GetString(Dictionary<string,string> queryParameters, bool authenticated = true, [System.Runtime.CompilerServices.CallerMemberName] string methodName = "")
+		{
+			var path = GetType().GetMethod(methodName).GetCustomAttributes(true).OfType<PathAttribute>().FirstOrDefault()?.Value;
+			if(string.IsNullOrWhiteSpace(path))
+				throw new Exception("Missing Path Attribute");
+
+			path = CombineUrl(path, queryParameters);
+			return GetString(path, authenticated);
+		}
+
+		/// <summary>
+		/// Gets the string content from the url provided from  PathAttribute on the method name
+		/// </summary>
+		/// <param name="authenticated"></param>
+		/// <param name="methodName"></param>
+		/// <returns></returns>
+		public virtual Task<string> GetString(bool authenticated = true, [System.Runtime.CompilerServices.CallerMemberName] string methodName = "")
+		{
+			var path = GetType().GetMethod(methodName).GetCustomAttributes(true).OfType<PathAttribute>().FirstOrDefault()?.Value;
+			if (string.IsNullOrWhiteSpace(path))
+				throw new Exception("Missing Path Attribute");
+			return GetString(path, authenticated);
+		}
 
 		public async virtual Task<string> GetString(string path, bool authenticated = true)
 		{
-			var resp = await GetMessage(path,authenticated);
+			var resp = await GetMessage(path,authenticated: authenticated);
 			return await resp.Content.ReadAsStringAsync();
 		}
 
+		public async virtual Task<string> GetString(string path, Dictionary<string, string> queryParameters,
+			Dictionary<string, string> headers, bool authenticated = true)
+		{
+			var resp = await GetMessage(path,headers, authenticated: authenticated);
+			return await resp.Content.ReadAsStringAsync();
+		}
 		public virtual async Task<string> PostUrl(string path, string content, string mediaType = "text/json", bool authenticated = true)
 		{
 			var message = await PostMessage(path,new StringContent(content, System.Text.Encoding.UTF8, mediaType),authenticated);
 			return await message.Content.ReadAsStringAsync();
-		}
-
-
-		public virtual async Task<T> Get<T>(string path, string id = "", bool authenticated = true)
-		{
-			var data = await GetString(Path.Combine(path, id),authenticated);
-			return await Task.Run(() => Deserialize<T>(data));
-
 		}
 
 		public virtual async Task<T> Post<T>(string path, string content, bool authenticated = true)
@@ -206,12 +266,32 @@ namespace SimpleAuth
 			return await Client.PutAsync(path, content);
 		}
 
-		public async Task<HttpResponseMessage> GetMessage(string path, bool authenticated = true)
+		public async Task<HttpResponseMessage> GetMessage(string path, Dictionary<string,string> headers = null,  bool authenticated = true)
+		{
+			path = await PrepareUrl(path);
+			var uri = BaseAddress != null ? new Uri(BaseAddress, path.TrimStart('/')) : new Uri(path);
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = uri,
+			};
+
+			if (headers?.Any() == true)
+			{
+				foreach (var header in headers)
+				{
+					request.Headers.Add(header.Key, header.Value);
+				}
+			}
+
+            return await SendMessage(request,authenticated);
+		}
+
+		public async Task<HttpResponseMessage> SendMessage(HttpRequestMessage message, bool authenticated = true)
 		{
 			if (authenticated)
 				await VerifyCredentials();
-			path = await PrepareUrl(path);
-            return await Client.GetAsync(path);
+			return await Client.SendAsync(message);
 		}
 
 		protected virtual async Task VerifyCredentials()
@@ -245,6 +325,48 @@ namespace SimpleAuth
 		protected virtual string SerializeObject(object obj)
 		{
 			return Newtonsoft.Json.JsonConvert.SerializeObject(obj);
+		}
+
+		public virtual string CombineUrl(string url, Dictionary<string, string> queryParameters)
+		{
+			if (queryParameters?.Any() != true)
+				return url;
+
+			var uri = BaseAddress != null ? new Uri(BaseAddress, url.TrimStart('/')) : new Uri(url);
+
+			var query = uri.Query;
+			var simplePath = string.IsNullOrWhiteSpace(query) ? url : url.Replace(query, "");
+
+			var parameters = HttpUtility.ParseQueryString(query);
+
+			foreach (var queryParameter in queryParameters)
+			{
+				var pathKey = "{" + queryParameter.Key + "}";
+				if (simplePath.Contains(pathKey))
+					simplePath = simplePath.Replace(pathKey, queryParameter.Value);
+				else
+					parameters[queryParameter.Key] = queryParameter.Value;
+			}
+			var newQuery = parameters.ToString();
+			var newPath = $"{simplePath}?{newQuery}";
+			return newPath;
+		}
+
+		public virtual Dictionary<string, string> GetHeadersFromMethod(MethodInfo method)
+		{
+			var headers = new Dictionary<string,string>();
+			var accepts = GetValueFromAttribute<AcceptsAttribute>(method);
+			if (!string.IsNullOrWhiteSpace(accepts))
+				headers["Accept"] = accepts;
+			var contentType = GetValueFromAttribute<ContentTypeAttribute>(method);
+			if (!string.IsNullOrWhiteSpace(contentType))
+				headers["Content-Type"] = contentType;
+			return headers;
+		}
+
+		public string GetValueFromAttribute<T>(MethodInfo method) where T : StringValueAttribute
+		{
+			return method.GetCustomAttributes(true).OfType<T>().FirstOrDefault()?.Value;
 		}
 	}
 }
