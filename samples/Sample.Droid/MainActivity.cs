ed9c624d505ebafaf938ce06211c7bab42bcb4a0
Jonathan Dick <jondick@gmail.com>
2016-11-24 20:01:31 -0500

    Native Google and Facebook Providers for Android (#19)
    * Facebook Native Android Login
    
    Added support for using the native Android Facebook SDK for Login
    
    * Prep Google authenticator for native support
    
    * Add Native Google Login Support to Android
    
    * Update Android Sample for Native providers
    
    Include some example code for using Facebook and Google Native login providers

--- samples/Sample.Droid/MainActivity.cs
+++ samples/Sample.Droid/MainActivity.cs
@@ -7,37 +7,98 @@ using Android.Widget;
 using Android.OS;
 using SimpleAuth;
 using SimpleAuth;
+using Android.Support.V4.App;
+using System.Threading.Tasks;
+
+[assembly: MetaData("com.facebook.sdk.ApplicationId", Value = "@string/facebook_app_id")]
 
 namespace Sample.Droid
 {
-	[Activity(Label = "Sample.Droid", MainLauncher = true, Icon = "@drawable/icon")]
-	public class MainActivity : Activity
-	{
-		int count = 1;
+    // We need to give the activity an explicit name for our acvitity that we provide to Facebook when we setup the app
+    // in the dev console, so it can callback to the right deep link.
+    [Activity(Label = "Sample.Droid", MainLauncher = true, Icon = "@drawable/icon", Name = "com.simpleauth.sample.MainActivity")]
+    public class MainActivity : FragmentActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
 
-		OAuthApi api;
-		protected override void OnCreate(Bundle bundle)
-		{
-			base.OnCreate(bundle);
+            // Initialize our native providers
+            SimpleAuth.Providers.Google.Init(this.Application);
+            SimpleAuth.Providers.Facebook.Init(this.Application, false);
 
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
-			api = new OAuthApi("google",new OAuthAuthenticator(
-				"authUrl",
-				"tokenUrl",
-				"redirecturl",
-				"clientid",
-				"clientsecret"));
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.MyButton);
-			button.Text = "Login";
-			button.Click += async delegate
-			{
-				var account = await api.Authenticate();
-				Console.WriteLine(account.Identifier);
-			};
-		}
-	}
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            FindViewById<Button>(Resource.Id.loginGoogleNative).Click += async (sender, e) =>
+            {
+                // Create an OAuth credential and use its clientId
+                var clientId = "28116949007-uho7tcbsm17l8uq7t96fk8i9d6ftugnd"; // Provide without the '.apps.googleusercontent.com' suffix
+
+                var google = new SimpleAuth.Providers.GoogleApi("google", clientId, "native") {
+                    Scopes = new [] { "https://www.googleapis.com/auth/userinfo.profile" }
+                };
+
+                var account = await AuthAsync(google);
+
+                // .. Do something with account
+            };
+
+            FindViewById<Button>(Resource.Id.loginFacebookNative).Click += async (sender, e) =>
+            {
+                // Use the App ID
+                var clientId = "1475840585777919";
+
+                var fb = new SimpleAuth.Providers.FacebookApi("facebook", clientId, "native");
+
+                var account = await AuthAsync(fb);
+
+                // .. Do something with account
+            };
+
+            FindViewById<Button>(Resource.Id.loginGeneric).Click += async (sender, e) =>
+            {
+                var oauth = new OAuthApi("someprovider", new OAuthAuthenticator(
+                    "authUrl",
+                    "tokenUrl",
+                    "redirecturl",
+                    "clientid",
+                    "clientsecret"));
+                    
+                var account = await AuthAsync(oauth);
+
+                // .. Do something with account
+            };
+        }
+
+        async Task<Account> AuthAsync(OAuthApi api)
+        {
+            Account result = null;
+            try
+            {
+                result = await api.Authenticate(;
+                Toast.MakeText(this, "Successfully Authenticated!", ToastLength.Long).Show();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Toast.MakeText(this, "Authentication Faild!", ToastLength.Long).Show();
+            }
+
+            if (result != null)
+                Console.WriteLine(await result.ToJsonAsync());
+
+            return result;
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            SimpleAuth.Providers.Facebook.OnActivityResult(requestCode, resultCode, data);
+
+            SimpleAuth.Providers.Google.OnActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
 
---
+++ src/SimpleAuth.Facebook.Droid/Facebook.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Runtime;
+using Xamarin.Facebook;
+using Xamarin.Facebook.Login;
+
+namespace SimpleAuth.Providers
+{
+    public class Facebook
+    {
+        public static void Init(Android.App.Application app, bool requestPublishPermissions)
+        {
+            RequestPublishPermissions = requestPublishPermissions;
+
+            app.RegisterActivityLifecycleCallbacks(activityLifecycleManager);
+
+            FacebookApi.IsUsingNative = true;
+            FacebookApi.ShowFacebookAuthenticator = Login;
+        }
+
+        public static void Uninit(Android.App.Application app)
+        {
+            app.UnregisterActivityLifecycleCallbacks(activityLifecycleManager);
+        }
+
+        public static void OnActivityResult(int requestCode, Result resultCode, Android.Content.Intent data)
+        {
+            callbackManager?.OnActivityResult(requestCode, (int)resultCode, data);
+        }
+
+        static ActivityLifecycleManager activityLifecycleManager = new ActivityLifecycleManager ();
+
+        public static bool RequestPublishPermissions { get; set; }
+
+        static ICallbackManager callbackManager = null;
+
+        public static async void Login(WebAuthenticator authenticator)
+        {
+            var fbAuthenticator = authenticator as FacebookAuthenticator;
+
+            var currentActivity = activityLifecycleManager.CurrentActivity;
+
+            FacebookSdk.SdkInitialize(currentActivity);
+
+            callbackManager = CallbackManagerFactory.Create();
+            var loginManager = LoginManager.Instance;
+            var fbHandler = new FbCallbackHandler();
+
+            loginManager.RegisterCallback(callbackManager, fbHandler);
+
+            fbHandler.Reset();
+
+            if (RequestPublishPermissions)
+                loginManager.LogInWithPublishPermissions(currentActivity, fbAuthenticator.Scope);
+            else
+                loginManager.LogInWithReadPermissions(currentActivity, fbAuthenticator.Scope);
+
+            LoginResult result = null;
+
+            try
+            {
+                result = await fbHandler.Task;
+            }
+            catch (Exception ex)
+            {
+                fbAuthenticator.OnError(ex.Message);
+                return;
+            }
+
+            if (result == null)
+                fbAuthenticator.OnCancelled();
+            
+            DateTime? expires = null;
+            long expiresIn = -1;
+            if (result?.AccessToken.Expires != null)
+            {
+                expires = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddMilliseconds(result.AccessToken.Expires.Time);
+                expiresIn = (long)(expires.Value - DateTime.Now).TotalSeconds;
+            }
+
+            fbAuthenticator.OnRecievedAuthCode(result?.AccessToken.Token, expiresIn);
+        }
+    }
+
+    class FbCallbackHandler : Java.Lang.Object, IFacebookCallback
+    {
+        TaskCompletionSource<LoginResult> tcs = new TaskCompletionSource<LoginResult>();
+
+        public void Reset()
+        {
+            if (tcs != null && !tcs.Task.IsCompleted)
+            {
+                tcs.TrySetResult (null);
+            }
+
+            tcs = new TaskCompletionSource<LoginResult>();
+        }
+
+        public Task<LoginResult> Task
+        {
+            get
+            {
+                return tcs.Task;
+            }
+        }
+        public void OnCancel()
+        {
+            tcs.TrySetResult(null);
+        }
+
+        public void OnError(FacebookException ex)
+        {
+            tcs.TrySetException(new System.Exception(ex.Message));
+        }
+
+        public void OnSuccess(Java.Lang.Object data)
+        {
+            tcs.TrySetResult(data.JavaCast<LoginResult>());
+        }
+    }
+}
