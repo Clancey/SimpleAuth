1274f0f9d0e4b0a5d19c01c126eacc9cbab24738
James Clancey <james.clancey@gmail.com>
2017-01-10 17:14:27 -0900

    Added OAuth Unit tests!

---
+++ src/SimpleAuth.Tests/Helpers/InMemoryAuthStorage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleAuth.Tests
+{
+	public class InMemoryAuthStorage : IAuthStorage
+	{
+		public InMemoryAuthStorage ()
+		{
+		}
+
+		Dictionary<string, string> data = new Dictionary<string, string> ();
+		public string GetSecured (string identifier, string clientId, string clientSecret, string sharedGroup)
+		{
+			string item;
+			if (data.TryGetValue (GetId (identifier, clientId, clientSecret, sharedGroup), out item))
+				return item;
+			return null;
+		}
+
+		public void SetSecured (string identifier, string value, string clientId, string clientSecret, string sharedGroup)
+		{
+			data [GetId(identifier,clientId,clientSecret,sharedGroup)] = value;
+		}
+
+		string GetId (string identifier, string clientid, string clientSecret, string groupID)
+		=> $"{identifier} - {clientid} - {clientSecret} - {groupID}";
+
+
+		public void Reset ()
+		{
+			data.Clear ();
+		}
+	}
+}
---
+++ src/SimpleAuth.Tests/Helpers/RequestMessage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+namespace SimpleAuth.Tests
+{
+	public struct RequestMessage
+	{
+		public string Url { get; set; }
+		public HttpMethod Method { get; set; }
+		public string Content { get; set; }
+
+		public override bool Equals (object obj)
+		{
+			return obj?.ToString() == this.ToString();
+		}
+		public override int GetHashCode ()
+		{
+			return this.ToString ().GetHashCode ();
+		}
+
+		public override string ToString ()
+		{
+			return $"{{\"Url\":\"{Url}\",\"Method\":{{\"Method\":\"{Method}\"}},\"Content\":\"{Content}\"}}";
+		}
+	}
+
+	public class RequestResponse
+	{
+		public string Content { get; set; }
+		public string ContentType { get; set; }
+		public HttpStatusCode StatusCode { get; set; }
+		public string RequiredAuthToken { get; set; }
+	}
+}
