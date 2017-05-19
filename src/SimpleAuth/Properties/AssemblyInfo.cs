e30069c58e40d8999adacfd1965d8989b468ebf0
James Clancey <clancey@xamarin.com>
2017-05-18 22:39:38 -0800

    Updated the nugets

--- src/SimpleAuth/Properties/AssemblyInfo.cs
+++ src/SimpleAuth/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.36")]
-[assembly: AssemblyFileVersion("1.0.36")]
+[assembly: AssemblyVersion("1.0.37")]
+[assembly: AssemblyFileVersion("1.0.37")]
 [assembly: InternalsVisibleTo("SimpleAuth.OnePassword.iOS")]
