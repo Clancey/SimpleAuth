e781d787babd6a5513e0ac65447fa2e1d3f3b7ac
James Clancey <clancey@xamarin.com>
2017-05-22 22:22:37 -0800

    bumped versions

--- src/SimpleAuth/Properties/AssemblyInfo.cs
+++ src/SimpleAuth/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.39")]
-[assembly: AssemblyFileVersion("1.0.39")]
+[assembly: AssemblyVersion("1.0.40")]
+[assembly: AssemblyFileVersion("1.0.40")]
 [assembly: InternalsVisibleTo("SimpleAuth.OnePassword.iOS")]
