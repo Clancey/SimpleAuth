aa3330451af539ce5c196f4ad754c48daec86fe4
James Clancey <james.clancey@gmail.com>
2017-05-22 16:16:30 -0800

    Formating

--- samples/Sample.iOS/AppDelegate.cs
+++ samples/Sample.iOS/AppDelegate.cs
@@ -10,13 +10,12 @@ namespace Sample.iOS
 {
 	// The UIApplicationDelegate for the application. This class is responsible for launching the
 	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
-	[Register("AppDelegate")]
+	[Register ("AppDelegate")]
 	public class AppDelegate : UIApplicationDelegate
 	{
 		// class-level declarations
 
-		public override UIWindow Window
-		{
+		public override UIWindow Window {
 			get;
 			set;
 		}
@@ -47,7 +46,7 @@ namespace Sample.iOS
 				Console.WriteLine (e);
 			};
 			// create a new window instance based on the screen size
-			Window = new UIWindow(UIScreen.MainScreen.Bounds);
+			Window = new UIWindow (UIScreen.MainScreen.Bounds);
 
 			Window.RootViewController = new DialogViewController (new RootElement ("Simple Auth") {
 				new Section("Google Api"){
@@ -89,7 +88,7 @@ namespace Sample.iOS
 			});
 
 			// make the window visible
-			Window.MakeKeyAndVisible();
+			Window.MakeKeyAndVisible ();
 
 			return true;
 		}
@@ -99,33 +98,30 @@ namespace Sample.iOS
 				return true;
 			return false;
 		}
-		async Task RunWithSpinner(string status,Func<Task> task)
+		async Task RunWithSpinner (string status, Func<Task> task)
 		{
-			try{
-				using(new Spinner(status)){
-					await task.Invoke();
-					ShowAlert("Success","");
+			try {
+				using (new Spinner (status)) {
+					await task.Invoke ();
+					ShowAlert ("Success", "");
 				}
-			}
-			catch(TaskCanceledException){
-				ShowAlert("Canceled","");
-			}
-			catch(Exception ex)
-			{
-				ShowAlert("Error",ex.ToString());
+			} catch (TaskCanceledException) {
+				ShowAlert ("Canceled", "");
+			} catch (Exception ex) {
+				ShowAlert ("Error", ex.ToString ());
 			}
 		}
-		void ShowAlert(string title, string message)
+		void ShowAlert (string title, string message)
 		{
 			var alert = UIAlertController.Create (title, message, UIAlertControllerStyle.Alert);
 
-			alert.AddAction(UIAlertAction.Create("Ok",UIAlertActionStyle.Default,(a)=>{
+			alert.AddAction (UIAlertAction.Create ("Ok", UIAlertActionStyle.Default, (a) => {
 
 			}));
 			Window.RootViewController.PresentViewControllerAsync (alert, true);
 
 		}
-		public override void OnResignActivation(UIApplication application)
+		public override void OnResignActivation (UIApplication application)
 		{
 			// Invoked when the application is about to move from active to inactive state.
 			// This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) 
@@ -133,25 +129,25 @@ namespace Sample.iOS
 			// Games should use this method to pause the game.
 		}
 
-		public override void DidEnterBackground(UIApplication application)
+		public override void DidEnterBackground (UIApplication application)
 		{
 			// Use this method to release shared resources, save user data, invalidate timers and store the application state.
 			// If your application supports background exection this method is called instead of WillTerminate when the user quits.
 		}
 
-		public override void WillEnterForeground(UIApplication application)
+		public override void WillEnterForeground (UIApplication application)
 		{
 			// Called as part of the transiton from background to active state.
 			// Here you can undo many of the changes made on entering the background.
 		}
 
-		public override void OnActivated(UIApplication application)
+		public override void OnActivated (UIApplication application)
 		{
 			// Restart any tasks that were paused (or not yet started) while the application was inactive. 
 			// If the application was previously in the background, optionally refresh the user interface.
 		}
 
-		public override void WillTerminate(UIApplication application)
+		public override void WillTerminate (UIApplication application)
 		{
 			// Called when the application is about to terminate. Save data, if needed. See also DidEnterBackground.
 		}
