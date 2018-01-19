c62bafbc3dea09719b19cfeb3bc74b243fa40262
James Clancey <james.clancey@gmail.com>
2018-01-19 12:05:54 -0900

    Added SimpleAuthCallbackActivity

---
+++ src/SimpleAuth.Droid.CustomTabs/SimpleAuthCallbackActivity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace SimpleAuth.Droid.CustomTabs
+{
+    public class SimpleAuthCallbackActivity : Activity
+    {
+        protected override void OnCreate(Android.OS.Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            Native.OnActivityResult(0, Result.Ok, this.Intent);
+            Finish();
+        }
+    }
+}
