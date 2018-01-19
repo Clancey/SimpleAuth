a199922876a2677ddb30beb15657b7fe26a80a03
James Clancey <james.clancey@gmail.com>
2018-01-19 11:36:32 -0900

    Started on custom tabs

---
+++ src/SimpleAuth.Droid.CustomTabs/ActivityLifecycleCallbackManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Support.V4.App;
+namespace SimpleAuth
+{
+    
+    internal class ActivityLifecycleCallbackManager : Java.Lang.Object, global::Android.App.Application.IActivityLifecycleCallbacks
+    {
+        public FragmentActivity CurrentActivity { get; private set; }
+
+        public void OnActivityCreated(Activity activity, Bundle savedInstanceState)
+        {
+            CurrentActivity = activity as FragmentActivity;
+        }
+
+        public void OnActivityDestroyed(Activity activity)
+        {
+            CurrentActivity = null;
+        }
+
+        public void OnActivityPaused(Activity activity)
+        {
+        }
+
+        public void OnActivityResumed(Activity activity)
+        {
+            CurrentActivity = activity as FragmentActivity;
+        }
+
+        public void OnActivitySaveInstanceState(Activity activity, Bundle outState)
+        {
+        }
+
+        public void OnActivityStarted(Activity activity)
+        {
+            CurrentActivity = activity as FragmentActivity;
+        }
+
+        public void OnActivityStopped(Activity activity)
+        {
+        }
+    }
+}
---
+++ src/SimpleAuth.Droid.CustomTabs/NativeCustomTabsAuthenticator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.CustomTabs;
+using Android.Views;
+using Android.Widget;
+
+namespace SimpleAuth
+{
+   public class NativeCustomTabsAuthenticator
+    {
+        static ActivityLifecycleCallbackManager activityLifecycleManager;
+        static Dictionary<string, CustomTabsAuthSession> authenticators = new Dictionary<string, CustomTabsAuthSession>();
+        public static bool IsActivated { get; private set; }
+        public static void Activate(Android.App.Application app)
+        {
+            Native.RegisterCallBack("CustomTabs", OnActivityResult);
+            OAuthApi.ShowAuthenticator = ShowAuthenticator;
+
+            if (activityLifecycleManager == null)
+            {
+                activityLifecycleManager = new ActivityLifecycleCallbackManager();
+                app.RegisterActivityLifecycleCallbacks(activityLifecycleManager);
+            }
+            IsActivated = true;
+        }
+
+
+        public static bool OnActivityResult(int requestCode, Result resultCode, Android.Content.Intent intent)
+        {
+            if (intent == null)
+                return false;
+
+            var uri = new Uri(intent.Data.ToString());
+
+            // Only handle schemes we expect
+            var scheme = uri.Scheme;
+
+            if (!authenticators.TryGetValue(scheme, out var authenticator))
+                return false;
+            return authenticator.Authenticator.CheckUrl(uri, null);
+        }
+
+        public static async void ShowAuthenticator(WebAuthenticator authenticator)
+        {
+            await BeginAuthentication(authenticator);
+        }
+
+        static async Task BeginAuthentication(WebAuthenticator authenticator)
+        {
+            try
+            {
+                var uri = (await authenticator.GetInitialUrl());
+                string redirectUrl = uri.GetParameter("redirect_uri");
+                var scheme = new Uri(redirectUrl).Scheme;
+
+                var authSession = new CustomTabsAuthSession
+                {
+                    Authenticator = authenticator,
+                    ParentActivity = activityLifecycleManager.CurrentActivity,
+                };
+
+                authenticators[scheme] = authSession;
+                authSession.CustomTabsActivityManager = new CustomTabsActivityManager(authSession.ParentActivity);
+                authSession.CustomTabsActivityManager.NavigationEvent += (navigationEvent, extras) =>
+                {
+                    if (navigationEvent == CustomTabsCallback.TabHidden)
+                    {
+                        authenticator.OnCancelled();
+                    }
+
+                };
+                authSession.CustomTabsActivityManager.CustomTabsServiceConnected += delegate
+                {
+                    var builder = new CustomTabsIntent.Builder(authSession.CustomTabsActivityManager.Session)
+                                                      .SetShowTitle(true);
+
+                    var customTabsIntent = builder.Build();
+                    customTabsIntent.Intent.AddFlags(Android.Content.ActivityFlags.SingleTop | ActivityFlags.NoHistory | ActivityFlags.NewTask);
+
+                    CustomTabsHelper.AddKeepAliveExtra(authSession.ParentActivity, customTabsIntent.Intent);
+
+                    customTabsIntent.LaunchUrl(authSession.ParentActivity, Android.Net.Uri.Parse(uri.AbsoluteUri));
+                };
+
+                if (!authSession.CustomTabsActivityManager.BindService())
+                    authenticator.OnError("CustomTabs not supported.");
+
+            }
+            catch (Exception ex)
+            {
+                authenticator.OnError(ex.Message);
+            }
+        }
+        class CustomTabsAuthSession
+        {
+
+            public CustomTabsActivityManager CustomTabsActivityManager { get; set; }
+            public Android.Support.V4.App.FragmentActivity ParentActivity { get; set; }
+            public WebAuthenticator Authenticator { get; set; }
+        }
+
+    }
+}
