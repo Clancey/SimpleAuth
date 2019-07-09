5280e00d75ad95b20602a8942fdd6b8fbe7b5f01
allan-chin <30632583+allan-chin@users.noreply.github.com>
2019-07-09 15:04:49 -0700

    Instagram Issue Fix
    Create a Custom WebAuthenticatorBroker for Instagram UI issue on IE.

---
+++ src/SimpleAuth.UWP/CustomWebAuthenticationResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Security.Authentication.Web;
+
+namespace SimpleAuth.UWP
+{
+    /// <summary>
+    /// Indicates the result of the authentication operation.
+    /// </summary>
+    // https://github.com/inthehand/Authful/blob/master/WebAuthenticationResult.cs
+    public sealed class WebAuthenticationResult
+    {
+        internal WebAuthenticationResult(string response, uint errorDetail, WebAuthenticationStatus responseStatus)
+        {
+            ResponseData = response;
+            ResponseErrorDetail = errorDetail;
+            ResponseStatus = responseStatus;
+        }
+
+        /// <summary>
+        /// Contains the protocol data when the operation successfully completes.
+        /// </summary>
+        public string ResponseData { get; private set; }
+
+        /// <summary>
+        /// Returns the HTTP error code when <see cref="ResponseStatus"/> is equal to <see cref="WebAuthenticationStatus.ErrorHttp"/>. 
+        /// This is only available if there is an error.
+        /// </summary>
+        public uint ResponseErrorDetail { get; private set; }
+
+        /// <summary>
+        /// Contains the status of the asynchronous operation when it completes.
+        /// </summary>
+        public WebAuthenticationStatus ResponseStatus { get; private set; }
+    }
+}
--- src/SimpleAuth.UWP/WebAuthenticatorWebView.cs
+++ src/SimpleAuth.UWP/WebAuthenticatorWebView.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleAuth.Providers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,15 +20,36 @@ namespace SimpleAuth.UWP
 		public async Task ShowAsync()
 		{
 			var url = await authenticator.GetInitialUrl();
-			var result = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, url, authenticator.RedirectUrl);
-			if (result.ResponseStatus == WebAuthenticationStatus.UserCancel)
-			{
-				authenticator.OnCancelled();
-				return;
-			}
-			if (result.ResponseStatus == WebAuthenticationStatus.ErrorHttp)
-				throw new Exception(result.ResponseErrorDetail.ToString());
-			authenticator.CheckUrl(new Uri(result.ResponseData), new System.Net.Cookie[0]);
-		}
-	}
+
+            // Thw Windows WebAuthenticatorBroker uses the IE browser which doesn't behave well
+            // for at least the Instagram authenticator. So, here we're using a custom borker.
+            // https://peterfoot.net/2019/03/13/webauthenticationbroker-and-github/
+            if (authenticator is InstagramAuthenticator)
+            {
+                // result here is of type SimpleAuth.UWP.WebAuthenticationResult
+                var result = await CustomWebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, url, authenticator.RedirectUrl);
+                if (result.ResponseStatus == WebAuthenticationStatus.UserCancel)
+                {
+                    authenticator.OnCancelled();
+                    return;
+                }
+                if (result.ResponseStatus == WebAuthenticationStatus.ErrorHttp)
+                    throw new Exception(result.ResponseErrorDetail.ToString());
+                authenticator.CheckUrl(new Uri(result.ResponseData), new System.Net.Cookie[0]);
+            }
+            else
+            {
+                // result here is of type Windows.Security.Authentication.Web.WebAuthenticationResult
+                var result = await WebAuthenticationBroker.AuthenticateAsync(WebAuthenticationOptions.None, url, authenticator.RedirectUrl);
+                if (result.ResponseStatus == WebAuthenticationStatus.UserCancel)
+                {
+                    authenticator.OnCancelled();
+                    return;
+                }
+                if (result.ResponseStatus == WebAuthenticationStatus.ErrorHttp)
+                    throw new Exception(result.ResponseErrorDetail.ToString());
+                authenticator.CheckUrl(new Uri(result.ResponseData), new System.Net.Cookie[0]);
+            }
+        }
+    }
 }
