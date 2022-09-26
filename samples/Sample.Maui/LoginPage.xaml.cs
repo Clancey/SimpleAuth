b3473841e5ef90942fde67ddbf573ec12410fbcf
jonlipsky <jonlipsky@github.com>
2022-09-26 16:35:31 -0700

    Added a Maui sample. Fixed maccatalyst compilation issue. (#127)
    * Add idea folder to gitignore
    
    * Added a Maui sample. Fixed maccatalyst compilation issue.

---
+++ samples/Sample.Maui/LoginPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using SimpleAuth;
+
+namespace Sample.Maui;
+
+public partial class LoginPage : ContentPage
+{
+    IBasicAuthenicator authenticator;
+    public LoginPage(IBasicAuthenicator authenticator)
+    {
+        this.authenticator = authenticator;
+        InitializeComponent();
+    }
+
+    async void Handle_Clicked(object sender, System.EventArgs e)
+    {
+        if (string.IsNullOrWhiteSpace(Username.Text))
+        {
+            await this.DisplayAlert("Error", "Username is invalid", "Ok");
+            return;
+        }
+
+        if (string.IsNullOrWhiteSpace(Password.Text))
+        {
+            await this.DisplayAlert("Error", "Password is invalid", "Ok");
+            return;
+        }
+
+        try
+        {
+            bool success = await authenticator.VerifyCredentials(Username.Text, Password.Text);
+
+            if (success)
+                await this.Navigation.PopModalAsync();
+            else
+                await this.DisplayAlert("Error", "Invalid credentials", "Ok");
+
+        }
+        catch (Exception ex)
+        {
+            await this.DisplayAlert("Error", ex.Message, "Ok");
+        }
+    }
+    protected override bool OnBackButtonPressed()
+    {
+        authenticator.OnCancelled();
+        return base.OnBackButtonPressed();
+    }
+}
+
+
